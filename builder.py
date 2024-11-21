36ca3157c507dd85ca54a7ccd78d2803afc811a8
Cédric Morelle <toucouleur@gmail.com>
2024-11-21 07:34:50 +0100

    feat: Update builder

--- builder.py
+++ builder.py
@@ -2,36 +2,51 @@
 
 import subprocess
 import platform
+import sys
+import os
+import PyQt6.QtCore
 
-from constants import (
-    version_number
-)
+from constants import version_number
 
 app_name = f"Wait & Pounce v{version_number}"
 
+# Common options for PyInstaller
+common_options = [
+    sys.executable,
+    "-m", "PyInstaller",
+    "--clean",
+    f"--name={app_name}",
+    "pounce_gui.pyw"
+]
+
+# Get the path to the QtMultimedia plugins
+qt_plugins_path = os.path.join(
+    PyQt6.QtCore.QLibraryInfo.path(PyQt6.QtCore.QLibraryInfo.LibraryPath.PluginsPath),
+    'multimedia'
+)
+
 if platform.system() == 'Windows':
-    pyinstaller_cmd = [
-        "pyinstaller",
+    pyinstaller_cmd = common_options + [
         "--onefile",
-        "--collect-submodules",
-        "PIL",
+        "--collect-submodules", "PIL",
         "--icon=pounce.ico",
         "--add-data=pounce.ico;.",
-        "--add-data=sounds:sounds",
-        "--add-data=cty.xml:.",
-        f"--name={app_name}",
-        "pounce_gui.pyw"
+        "--add-data=sounds;sounds",
+        "--add-data=cty.xml;.",
+        f'--add-binary={qt_plugins_path};PyQt6/Qt6/plugins/multimedia',
+        '--hidden-import=Foundation',
+        '--hidden-import=objc',
     ]
 elif platform.system() == 'Darwin':
-    pyinstaller_cmd = [
-        "pyinstaller",
+    pyinstaller_cmd = common_options + [
         "--windowed",
         "--icon=pounce.icns",
-        "--hidden-import=sip",
         "--add-data=sounds:sounds",
         "--add-data=cty.xml:.",
-        f"--name={app_name}",
-        "pounce_gui.pyw"
+        f'--add-binary={qt_plugins_path}:PyQt6/Qt6/plugins/multimedia',
+        '--hidden-import=Foundation',
+        '--hidden-import=objc',
     ]
 
+# Run the PyInstaller command
 subprocess.run(pyinstaller_cmd)
