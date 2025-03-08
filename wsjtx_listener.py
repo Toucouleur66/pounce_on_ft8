df8fc34400acdf921ee0e64a286ebea957158ade
Cédric Morelle <toucouleur@gmail.com>
2025-03-08 16:05:59 +0100

    fix: Try to make sure we never set self.server_status to None

--- wsjtx_listener.py
+++ wsjtx_listener.py
@@ -260,7 +260,7 @@ class Listener:
 
                 self.origin_addr = origin_addr
 
-                if server_status != self.server_status:
+                if server_status and server_status != self.server_status:
                     self.server_status = server_status
                     self.update_settings()                    
                     if self.message_callback:
@@ -652,21 +652,21 @@ class Listener:
         return int(suggested_freq)
     
     def handle_settings_packet(self):
+        log.info(f"SettingsPacket received: {self.master_slave_settings}")
         try:
             self.master_slave_settings = json.loads(self.the_packet.settings_json)              
-            if self.enable_log_packet_data:
-                log.info(f"SettingsPacket received: {self.master_slave_settings}")
-
             if (
                 self.last_master_slave_settings is None and
                 self.last_master_slave_settings != self.master_slave_settings
             ):
+                
                 self.last_master_slave_settings = self.master_slave_settings
                 if self.message_callback:
                     self.message_callback({
                         'type'     : 'master_slave_settings',
                         'settings' : self.master_slave_settings
                     })
+                log.info(f"SettingsPacket received and sent to GUI")                    
         except Exception as e:
             log.error(f"Error processing SettingsPacket: {e}")     
 
