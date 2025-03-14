f5d7f0c730401577d3379a56ff06050d9b5dce70
Cédric Morelle <toucouleur@gmail.com>
2025-03-14 12:13:35 +0100

    feat: Remove time.sleep and use QTimer to try to avoid GUI freezing

--- worker.py
+++ worker.py
@@ -1,10 +1,6 @@
-# worker.py
-
-import time
 import traceback
-
 from datetime import datetime
-from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
+from PyQt6.QtCore import QObject, pyqtSignal, QTimer
 
 from wsjtx_listener import Listener
 
@@ -15,76 +11,79 @@ class Worker(QObject):
     message                         = pyqtSignal(object)
     update_listener_settings_signal = pyqtSignal()
     send_settings_signal            = pyqtSignal()
-    reset_settings_signal           = pyqtSignal()    
+    reset_settings_signal           = pyqtSignal()
 
     def __init__(
             self,
-            monitoring_settings,
-            mode,
+            monitoring_settings, 
+            mode, 
             stop_event,
-            primary_udp_server_address,
+            primary_udp_server_address, 
             primary_udp_server_port,
-            secondary_udp_server_address,
-            secondary_udp_server_port,
+            secondary_udp_server_address, 
+            secondary_udp_server_port, 
             enable_secondary_udp_server,
-            logging_udp_server_address,
+            logging_udp_server_address, 
             logging_udp_server_port,
-            enable_logging_udp_server,            
-            enable_sending_reply,
-            max_reply_attemps_to_callsign,
+            enable_logging_udp_server, 
+            enable_sending_reply, 
+            max_reply_attemps_to_callsign, 
             max_working_delay,
-            enable_log_all_valid_contact,
-            enable_gap_finder,
+            enable_log_all_valid_contact, 
+            enable_gap_finder, 
             enable_watchdog_bypass,
-            enable_debug_output,
-            enable_pounce_log,
+            enable_debug_output, 
+            enable_pounce_log, 
             enable_log_packet_data,
-            adif_file_path,
-            adif_worked_backup_file_path,
+            adif_file_path, 
+            adif_worked_backup_file_path, 
             worked_before_preference,
             enable_marathon,
-            marathon_preference                          
+            marathon_preference
         ):
-        super(Worker, self).__init__()
+        
+        super().__init__()
 
         self.update_listener_settings_signal.connect(self.update_listener_settings)
         self.send_settings_signal.connect(self.send_master_settings)
         self.reset_settings_signal.connect(self.reset_slave_settings)
 
-        self.listener                           = None 
-        self.stop_event                         = stop_event
+        self.listener = None
+        self.stop_event = stop_event
 
-        self.mode                               = mode
-        self.monitoring_settings                = monitoring_settings                
-            
-        self.primary_udp_server_address         = primary_udp_server_address
-        self.primary_udp_server_port            = primary_udp_server_port
+        self.mode = mode
+        self.monitoring_settings = monitoring_settings
 
-        self.secondary_udp_server_address       = secondary_udp_server_address
-        self.secondary_udp_server_port          = secondary_udp_server_port
-        self.enable_secondary_udp_server        = enable_secondary_udp_server                            
+        self.primary_udp_server_address = primary_udp_server_address
+        self.primary_udp_server_port = primary_udp_server_port
 
-        self.logging_udp_server_address         = logging_udp_server_address
-        self.logging_udp_server_port            = logging_udp_server_port
-        self.enable_logging_udp_server          = enable_logging_udp_server                            
+        self.secondary_udp_server_address = secondary_udp_server_address
+        self.secondary_udp_server_port = secondary_udp_server_port
+        self.enable_secondary_udp_server = enable_secondary_udp_server
 
-        self.enable_sending_reply               = enable_sending_reply
-        self.max_reply_attemps_to_callsign      = max_reply_attemps_to_callsign
-        self.max_working_delay                  = max_working_delay
-        self.enable_log_all_valid_contact       = enable_log_all_valid_contact
-        self.enable_gap_finder                   = enable_gap_finder
-        self.enable_watchdog_bypass             = enable_watchdog_bypass
-        self.enable_debug_output                = enable_debug_output
-        self.enable_pounce_log                  = enable_pounce_log   
-        self.enable_log_packet_data             = enable_log_packet_data
-        
-        self.adif_file_path                      = adif_file_path
-        self.adif_worked_backup_file_path        = adif_worked_backup_file_path
-        self.worked_before_preference           = worked_before_preference
-        self.enable_marathon                    = enable_marathon
-        self.marathon_preference                = marathon_preference
+        self.logging_udp_server_address = logging_udp_server_address
+        self.logging_udp_server_port = logging_udp_server_port
+        self.enable_logging_udp_server = enable_logging_udp_server
 
-        self.last_update_time                   = datetime.now() 
+        self.enable_sending_reply = enable_sending_reply
+        self.max_reply_attemps_to_callsign = max_reply_attemps_to_callsign
+        self.max_working_delay = max_working_delay
+        self.enable_log_all_valid_contact = enable_log_all_valid_contact
+        self.enable_gap_finder = enable_gap_finder
+        self.enable_watchdog_bypass = enable_watchdog_bypass
+        self.enable_debug_output = enable_debug_output
+        self.enable_pounce_log = enable_pounce_log
+        self.enable_log_packet_data = enable_log_packet_data
+
+        self.adif_file_path = adif_file_path
+        self.adif_worked_backup_file_path = adif_worked_backup_file_path
+        self.worked_before_preference = worked_before_preference
+        self.enable_marathon = enable_marathon
+        self.marathon_preference = marathon_preference
+
+        self.last_update_time = datetime.now()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.check_stop_event)
 
     def run(self):
         try:
@@ -129,35 +128,37 @@ class Worker(QObject):
             )
             self.listener.listen()
             self.listener_started.emit()
-
-            while not self.stop_event.is_set():
-                time.sleep(0.1)
-            self.listener.stop()
-            self.listener.t.join()
+            
+            self.timer.start(100)  
+            
         except Exception as e:
             error_message = f"{e}\n{traceback.format_exc()}"
             self.error.emit(error_message)
-        finally:
+            self.stop()
+
+    def check_stop_event(self):
+        if self.stop_event.is_set():
+            self.timer.stop()
+            if self.listener is not None:
+                self.listener.stop()
+                self.listener.t.join()
             self.finished.emit()
 
     def stop(self):
-        if self.listener is not None:
-            self.listener.stop()            
+        self.stop_event.set()  
+        self.check_stop_event()
 
-    """
-        Make sure we are using the latest settings
-    """
     def update_listener_settings(self):
         current_time = datetime.now()
-        if (current_time - self.last_update_time).total_seconds() > 1:
+        if (current_time - self.last_update_time).total_seconds() >= 2:
             self.last_update_time = current_time
             if self.listener is not None:
                 self.listener.update_listener_settings()
 
     def send_master_settings(self):
         if self.listener is not None:
-            self.listener.send_settings_packet()            
+            self.listener.send_settings_packet()
 
     def reset_slave_settings(self):
         if self.listener is not None:
-            self.listener.reset_slave_settings()                        
+            self.listener.reset_slave_settings()
